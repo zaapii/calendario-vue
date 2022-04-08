a317bb202faf4515b05872aa6d1a07f11e820369
unknown <zaapii@gmail.com>
2022-04-08 20:46:54 -0300

    Navigation drawer ahora es un component para dejar mas limpio el App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,114 +1,29 @@
 <template>
   <v-theme-provider root>
     <v-app>
-      <v-navigation-drawer app>
-        <v-list-item>
-          <v-list-item-content>
-            <v-list-item-title class="text-h6">
-<!--               <img
-                v-if="$vuetify.theme.dark"
-                src="https://legalim.netlify.app/img/logomark.c9fe0c97.png"
-                alt=""
-                style="width: 90%"
-              /> -->
-               <img
-                src="https://www.seekpng.com/png/full/246-2467553_your-logo-here-png.png"
-                alt=""
-                style="max-width:50%;"
-              /> 
-<!--               <img
-                v-else
-                src="https://legalim.netlify.app/img/logomark.c9fe0c97.png"
-                style="width: 175%; transform: translateX(3rem)"
-                alt=""
-              /> -->
-            </v-list-item-title>
-            <v-list-item-subtitle> Calendar </v-list-item-subtitle>
-          </v-list-item-content>
-        </v-list-item>
-
-        <v-divider></v-divider>
-        <!-- ESTO ES UN ASCO PERO ANDA -->
-        <v-list dense nav>
-          <v-list-item v-if="!isAuthenticated">
-            <v-list-item-content>
-              <router-link to="/login">
-                <v-btn block elevation="2" outlined>
-                  <v-list-item-icon>
-                    <v-icon>mdi-login</v-icon>
-                  </v-list-item-icon>
-                  &nbsp;&nbsp;Ingresar&nbsp;&nbsp;&nbsp;</v-btn
-                ></router-link
-              >
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item>
-            <v-list-item-content>
-              <router-link to="/calendario">
-                <v-btn block elevation="2" outlined>
-                  <v-list-item-icon>
-                    <v-icon>mdi-calendar</v-icon>
-                  </v-list-item-icon>
-                  Calendario</v-btn
-                ></router-link
-              >
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item v-if="isAuthenticated">
-            <v-list-item-content>
-              
-              <v-btn @click="handleClickSignOut" block elevation="2" outlined
-                ><v-list-item-icon>
-              <v-icon>mdi-logout</v-icon>
-            </v-list-item-icon>Salir</v-btn
-              >
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-      </v-navigation-drawer>
-
+      
+      <v-navigation-drawer />
       <vAppBar />
-      <!-- Sizes your content based upon application components -->
       <v-main>
-        <!-- Provides the application the proper gutter -->
         <v-container fluid>
-          <!-- If using vue-router -->
           <router-view></router-view>
         </v-container>
       </v-main>
 
       <v-footer app>
-        <!-- -->
+        <!-- NADA POR AQUí POR AHORA -->
       </v-footer>
     </v-app>
   </v-theme-provider>
 </template>
 
 <script>
-import { mapActions, mapGetters } from "vuex";
 import vAppBar from "./components/vAppBar";
-import firebase from "firebase/compat/app";
-import "firebase/compat/auth";
-import "firebase/compat/firestore";
+import vNavigationDrawer from "./components/vNavigationDrawer";
 export default {
   components: {
     vAppBar,
-  },
-  computed: {
-    ...mapGetters(["isAuthenticated"]),
-  },
-  methods: {
-    ...mapActions(["logOutCurrentUser"]),
-    async handleClickSignOut() {
-      firebase
-        .auth()
-        .signOut()
-        .then(() => {
-          alert("Successfully signed out.");
-        });
-      this.logOutCurrentUser();
-      this.$router.push({ path: "/login" });
-    },
+    vNavigationDrawer
   },
 };
 </script>
---
+++ src/components/vNavigationDrawer.vue
@@ -0,0 +1,96 @@
+<template>
+  <v-navigation-drawer app>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title class="text-h6">
+<!--               <img
+                v-if="$vuetify.theme.dark"
+                src="https://legalim.netlify.app/img/logomark.c9fe0c97.png"
+                alt=""
+                style="width: 90%"
+              /> -->
+<!--               <img
+                v-else
+                src="https://legalim.netlify.app/img/logomark.c9fe0c97.png"
+                style="width: 175%; transform: translateX(3rem)"
+                alt=""
+              /> -->
+               <img
+                src="https://www.seekpng.com/png/full/246-2467553_your-logo-here-png.png"
+                alt=""
+                style="max-width:50%;"
+              /> 
+            </v-list-item-title>
+            <v-list-item-subtitle> Calendar </v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-divider></v-divider>
+        <!-- ESTO ES UN ASCO PERO ANDA -->
+        <v-list dense nav>
+          <v-list-item v-if="!isAuthenticated">
+            <v-list-item-content>
+              <router-link to="/login">
+                <v-btn block elevation="2" outlined>
+                  <v-list-item-icon>
+                    <v-icon>mdi-login</v-icon>
+                  </v-list-item-icon>
+                  &nbsp;&nbsp;Ingresar&nbsp;&nbsp;&nbsp;</v-btn
+                ></router-link
+              >
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-content>
+              <router-link to="/calendario">
+                <v-btn block elevation="2" outlined>
+                  <v-list-item-icon>
+                    <v-icon>mdi-calendar</v-icon>
+                  </v-list-item-icon>
+                  Calendario</v-btn
+                ></router-link
+              >
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item v-if="isAuthenticated">
+            <v-list-item-content>
+              
+              <v-btn @click="handleClickSignOut" block elevation="2" outlined
+                ><v-list-item-icon>
+              <v-icon>mdi-logout</v-icon>
+            </v-list-item-icon>Salir</v-btn
+              >
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+</template>
+
+<script>
+import { mapActions, mapGetters } from "vuex";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+export default {
+computed: {
+    ...mapGetters(["isAuthenticated"]),
+  },
+  methods: {
+    ...mapActions(["logOutCurrentUser"]),
+    async handleClickSignOut() {
+      firebase
+        .auth()
+        .signOut()
+        .then(() => {
+          alert("Successfully signed out.");
+        });
+      this.logOutCurrentUser();
+      this.$router.push({ path: "/login" });
+    },
+  },
+}
+</script>
+
+<style>
+
+</style>
